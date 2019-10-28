b3524bf290f8c0f187489746991d55b2ac83d8fc
github username <github email address>
2019-10-28 13:32:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+// import { WSAVERNOTSUPPORTED } from "constants";
+
+
+function writeCards(array, ocassion) {
+    thankYouCard = []
+    for (let i = 0; i < array.length; i ++) {
+        thankYouCard.push(`Thank you, ${array[i]}, for the wonderful ${ocassion} gift!`);
+    }
+    return (thankYouCard)
+}
+
+writeCards();
+
+function countDown(integer){
+    while (integer > 0){
+        console.log(integer)
+        integer -= 1
+    }
+    console.log(integer)
+}
+
+
